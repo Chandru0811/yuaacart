9c94e6b6a5c13545d2f57b36fb36d4bbef7ff544
keerthick07 <keerthickvasan08@gmail.com>
2024-05-09 11:32:14 +0530

    zoom effect update

--- src/pages/Discription.js
+++ src/pages/Discription.js
@@ -1,7 +1,5 @@
 import React, { useState, useEffect, useRef } from "react";
-// import Laptop from "../assets/laptop.jpg";
-// import product1 from "../assets/product1.webp";
-// import product2 from "../assets/product2.jpg";
+import ReactImageMagnify from 'react-image-magnify';
 import pro1 from "../assets/pro1.jpg";
 import pro2 from "../assets/pro2.jpg";
 import pro3 from "../assets/pro3.jpg";
@@ -9,7 +7,6 @@ import pro4 from "../assets/pro4.jpg";
 import pro5 from "../assets/pro5.jpg";
 import pro6 from "../assets/pro6.jpg";
 import { Link } from "react-router-dom";
-import ProductList from "./Products/ProductCarouselList";
 import { Tab, Tabs } from "react-bootstrap";
 
 export default function Discription() {
@@ -32,17 +29,17 @@ export default function Discription() {
   const handleChange = (image) => {
     setMainImage(image);
   };
+
   return (
     <div className="container-fluid">
-      <div className="container">
+      <div className="container mt-4 ">
         <h3>
           DELL LATITUDE 7390 TOUCH 8th Gen Intel® Core™ i7-8650U (Quad-Core, 8MB
           Cache, 4.20GHz) 8GB Ram I 512GB
         </h3>
         <div className="row m-5">
           <div className="col-md-6 col-12 ">
-            <div className="d-flex justify-content-center align-items-center "
-              style={{ minWidth: " 76%" }}>
+            <div className="d-flex justify-content-center align-items-center " style={{ minWidth: " 76%" }}>
               <div class="zoom-image">
                 <img
                   class="zoom-image__img"
@@ -56,7 +53,6 @@ export default function Discription() {
                 <div className="" ref={inlineContainerRef} style={{ width: "50px", height: "50px" }} />
                 <div className="" ref={paneRef} style={{ width: "50px", height: "50px" }} />
               </div>
-              {/* <img src={mainImage} alt="WWG" className="img-fluid "  style={{maxHeight: "95%"}}/> */}
             </div>
             <div
               className=" d-flex pt-3 gap-2"
@@ -100,9 +96,7 @@ export default function Discription() {
             </div>
           </div>
           <div className="col-md-6 col-12">
-
             <h4>
-              {" "}
               DELL LATITUDE 7390 TOUCH 8th Gen Intel® Core™ i7-8650U (Quad-Core,
               8MB Cache, 4.20GHz) 8GB Ram I 512GB
             </h4>
@@ -125,7 +119,6 @@ export default function Discription() {
               <Link to="/cart">
                 <button className="btn " style={{ backgroundColor: "rgb(0 107 255)", color: "white" }}>Add To Cart</button>
               </Link>
-
               <Link to="/checkout">
                 <button className="btn" style={{ backgroundColor: "#18b5fc", color: "white" }}>Buy Now</button>
               </Link>
@@ -136,8 +129,9 @@ export default function Discription() {
           defaultActiveKey="home"
           transition={false}
           id="noanim-tab-example"
-          className="mb-3">
-          <Tab eventKey="home" title="Description" >
+          className="mb-3"
+        >
+          <Tab eventKey="home" title="Description">
             <p>Here is the detailed specification of the laptop you provided:</p>
             <ul>
               <li>Processor: 8th Gen Intel Core i7-8650U (Quad-Core, 8MB Cache, 4.20GHz)</li>
@@ -155,13 +149,11 @@ export default function Discription() {
             <p>There are no reviews yet.</p>
           </Tab>
         </Tabs>
-
       </div>
-
-
       <div className="px-1 py-4">
         <h3>Related products</h3>
-        <ProductList />
+        {/* Assuming ProductList is defined elsewhere */}
+        {/* <ProductList /> */}
       </div>
     </div>
   );
